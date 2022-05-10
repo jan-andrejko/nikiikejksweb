fce0543a6449d561be394f3fd3e78d696b8be347
Ján Andrejko <88081799+jan-andrejko@users.noreply.github.com>
2022-05-10 21:46:10 +0200

    create services
    I created services with favorites cakes on the homepage.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service:OrderDetailsService) { }
+  cakeData:any;
   ngOnInit(): void {
+    this.cakeData = this.service.cakeDetails;
   }
 
 }
---
+++ src/app/services/order-details.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderDetailsService {
+
+  constructor() { }
+
+
+  // cakedetails
+  cakeDetails = [
+    {
+      id:1,
+      cakeName:"Cupcake",
+      cakeDetails: "makovy korpus visnovy confit s cream cheese kremom",
+      cakePrice: 1.6,
+      cakeImg:'assets/images/cupcake.jpg',
+    },
+    {
+      id:2,
+      cakeName:"Sladke pohariky",
+      cakeDetails: "New York cheesecake v pohariku",
+      cakePrice: 2,
+      cakeImg:'assets/images/sladke_pohariky.jpg',
+    },
+    {
+      id:3,
+      cakeName:"Mini pavlovky",
+      cakeDetails: "vanilkovy krem + dekoracia",
+      cakePrice: 2.5,
+      cakeImg:'assets/images/mini_pavlovky.jpg',
+    },
+    {
+      id:4,
+      cakeName:"Tartaletky",
+      cakeDetails: "biela cokolada s visnami",
+      cakePrice: 1.5,
+      cakeImg:'assets/images/tartaletky.jpg',
+    },
+    {
+      id:5,
+      cakeName:"Brownies",
+      cakeDetails: "karamel s pekanovymi orechmi",
+      cakePrice: 1.8,
+      cakeImg:'assets/images/brownies1.jpg',
+    },
+  ]
+
+}
