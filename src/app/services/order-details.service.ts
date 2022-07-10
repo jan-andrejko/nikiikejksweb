5c85a02334120bb1763e88fd7d5d9559dc58f21d
Ján Andrejko <88081799+jan-andrejko@users.noreply.github.com>
2022-07-10 20:43:57 +0200

    about page + menu page
    I added text to the about page and then added cakeDescription to the order-detail service.

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -12,9 +12,10 @@ export class OrderDetailsService {
   cakeDetails = [
     {
       id:1,
-      cakeName:"Cupcake",
-      cakeDetails: "makovy korpus visnovy confit s cream cheese kremom",
-      cakePrice: 1.6,
+      cakeName:"Torty 18cm priemer",
+      cakeDetails: "Tu nájdete torty s priemerom 18cm, kde je možné si vybrať s viacero príchutí.",
+      cakeDescription:"Náplne do tort robím na báze mascarpone + sľahačka v spojení s kvalitnou belgickou čokoládov Callebaut (biela, mliečna), týmto krémom obtieram aj boky torty ( v prípade záujmu aj možnosť výberu nahej torty - viditeľné korpusy). Do torty viem pridať karamel/slaný karamel, ktorý zvýrazní chuť a okrem neho je samozrejmosťou ovocie v každom poschodí (maliny, čučoriedky, jahody, lesné ovocie, višne).",
+      cakePrice: 55,
       cakeImg:'assets/images/cupcake.jpg',
     },
     {
